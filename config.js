c3ebf3c8041598f10ca7a36854274dc0fc837a7b
DILNETHU <142978322+DilnethuR@users.noreply.github.com>
2025-08-28 12:18:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined? "ENTER YOUR SESSION ID":process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined? "E3sVBLLS#ELrwSuOqiL1NTzbJ2llcRspXU_2fT2_L-s1YTMj-Z20":process.env.SESSION_ID,
 MONGODB: process.env.MONGODB || "mongodb://mongo:EWvuSLBqlBKLoFOXinNYdGqAhGHQKvUB@tramway.proxy.rlwy.net:29073",
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
 tokenm: process.env.GITHUB_TOKEN || "false",
